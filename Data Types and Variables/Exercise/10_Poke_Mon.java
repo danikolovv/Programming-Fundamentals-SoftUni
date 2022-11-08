38c291bfa74cc0657adbf60f568d163fbbba7a73
Daniel Nikolov <114946180+danikolovv@users.noreply.github.com>
2022-11-08 18:11:10 +0200

    Create 10_Poke_Mon.java

---
+++ Data Types and Variables/Exercise/10_Poke_Mon.java
@@ -0,0 +1,34 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class PokeMon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //с какво се характеризира нашия покемон --> power, distance, factor
+        //повтаряне
+        //изваждаме дистанцията от силата
+        // спираме, когато силата е по-малко от дистанцията
+        //продължава --> power >= distance
+
+        int power = Integer.parseInt(scanner.nextLine());
+        int distance = Integer.parseInt(scanner.nextLine());
+        int factor = Integer.parseInt(scanner.nextLine());
+
+        int firstPower = power;
+        int countPoke = 0;
+        while (power >= distance){
+            power -= distance;
+            countPoke++;
+
+            if (power == firstPower / 2){
+                if (factor != 0) {
+                    power = power / factor;
+                }
+            }
+        }
+        System.out.println(power);
+        System.out.println(countPoke);
+    }
+}
