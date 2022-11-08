8cae799a049d580304b0aad991fd5d466a941993
Daniel Nikolov <114946180+danikolovv@users.noreply.github.com>
2022-11-08 19:05:21 +0200

    Create 02_Grades.java

---
+++ Methods/Lab/02_Grades.java
@@ -0,0 +1,25 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class Grades {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        receiveGrade(Double.parseDouble(scanner.nextLine()));
+    }
+
+    public static void receiveGrade(double number){
+        if (number >= 2.00 && number <= 2.99){
+            System.out.println("Fail");
+        } else if (number >= 3.00 && number <= 3.49){
+            System.out.println("Poor");
+        } else if (number >= 3.50 && number <= 4.49){
+            System.out.println("Good");
+        } else if (number >= 4.50 && number <= 5.49){
+            System.out.println("Very good");
+        } else if (number >= 5.50 && number <= 6.00){
+            System.out.println("Excellent");
+        }
+    }
+}
