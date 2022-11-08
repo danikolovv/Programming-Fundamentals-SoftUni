be18b22afef813e986fc7c47cb67670ca07bda6a
Daniel Nikolov <114946180+danikolovv@users.noreply.github.com>
2022-11-08 18:14:10 +0200

    Create 01_Day_Of_Week.java

---
+++ Arrays/Lab/01_Day_Of_Week.java
@@ -0,0 +1,27 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class DayOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] dayOfWeekArr = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+        };
+
+        int day = Integer.parseInt(scanner.nextLine());
+
+        if (day >= 1 && day <= 7){
+            System.out.println(dayOfWeekArr[day-1]);
+        } else {
+            System.out.println("Invalid day!");
+        }
+    }
+}
