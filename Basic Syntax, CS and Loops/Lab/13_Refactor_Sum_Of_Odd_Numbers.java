7d9af551bc8fc21a31684ec6c6763a8aa1048e4d
Daniel Nikolov <114946180+danikolovv@users.noreply.github.com>
2022-11-14 21:26:50 +0200

    Update 13_Refactor_Sum_Of_Odd_Numbers.java

--- Basic Syntax, CS and Loops/Lab/13_Refactor_Sum_Of_Odd_Numbers.java
+++ Basic Syntax, CS and Loops/Lab/13_Refactor_Sum_Of_Odd_Numbers.java
@@ -1,1 +1,20 @@
-have to be done
+package Lab;
+
+import java.util.Scanner;
+
+public class RefactorSumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int sum = 1;
+        int finalSum = 0;
+        for (int j = 1; j <= n; j++) {
+            System.out.println(sum);
+            finalSum += sum;
+            sum += 2;
+        }
+        System.out.printf("Sum: %d", finalSum);
+    }
+}
